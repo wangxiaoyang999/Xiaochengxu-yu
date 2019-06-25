1100fa0ab80e3828d26435bee92ece30f4197e66
wangxiang13 <48750423+wangxiang13@users.noreply.github.com>
2019-06-25 18:47:10 +0800

    Update index.js

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -10,30 +10,56 @@ Page({
   },
 
   onLoad: function() {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
-      })
-      return
-    }
-
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
+    var me = this;
+    wx.getLocation({
+      type:'wgs84',
+      altitude:true,
+      success:function(res){
+        console.log("wx.getLocation.......");
+        console.log(res);
+        if(res&&res.latitude&&res.longitude){
+          var longitude = res.longitude, latitude = res.latitude;
+          me.loadCity(longitude,latitude);
+        }else {
+          me.setData({
+            city:'获取失败'
+          });
         }
-      }
+      },
+      fail:function(res){
+        me.setData({
+          city:'获取失败'
+        });
+      },
+      complete:function(res){},
     })
   },
+  
+      
+  loadCity: function(longitude,latitude){
+    var me = this;
+    wx.request({
+      url: 'https://api.map.baidu.com/geocoder/v2/?ak=1G4Ln9myGQPH95kRxeMi70C4X0zIZ2Z3&location=' + latitude + ',' + longitude + '&output=json',
+      data:{},
+      header:{
+        'Content-Type':'application/json'
+      },
+      success: function(res){
+        if(res && res.data){
+          var city = res.data.result.addressComponent.city;
+          console.log("res..................");
+          console.log(res);
+          me.setData({
+            city:city.indexOf('市') > -1 ? city.substr(0,city.indexOf('市')) :city
+          });
+        }else(
+          me.setData({
+            city:'获取失败'
+          });
+          }
+          }
+          })
+      },
 
   onGetUserInfo: function(e) {
     if (!this.logged && e.detail.userInfo) {
