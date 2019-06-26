fe3630d967b48196f4f8d92a553fe72dfa2d73a7
wangxiang13 <48750423+wangxiang13@users.noreply.github.com>
2019-06-26 19:37:03 +0800

    Add files via upload

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,76 +1,41 @@
 //index.js
+//获取应用实例
+var util_weather = require('../api/util.js')
+var formatLocation = util_weather.formatLocation
 const app = getApp()
 
 Page({
   data: {
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
+    weatherInfo: {},
+    newLocation: {},
+    lifeInfo: {},
+    nowInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-
-  onLoad: function() {
-    var me = this;
+  onReady: function () {
+    //初始化加载数据
+    var self = this
+    //获取定位信息 经纬度
     wx.getLocation({
-      type:'wgs84',
-      altitude:true,
-      success:function(res){
-        console.log("wx.getLocation.......");
-        console.log(res);
-        if(res&&res.latitude&&res.longitude){
-          var longitude = res.longitude, latitude = res.latitude;
-          me.loadCity(longitude,latitude);
-        }else {
-          me.setData({
-            city:'获取失败'
-          });
+      success: function (res) {
+        //初始化【北京】经纬度  location=39.93:116.40（格式是 纬度:经度，英文冒号分隔） 
+        var newLocation = '39.93:116.40';
+        if (res) { newLocation = res.latitude + ":" + res.longitude }
+        self.setData({
+          newLocation: newLocation
+        })
+        if (!self.data.nowInfo.now) {
+          //初始化获取 当前的天气状况
+          wx.request({
+            url: 'https://api.seniverse.com/v3/weather/now.json?key=fdw9qkun1btvenxt&location=' + newLocation + '&language=zh-Hans&unit=c',
+            success: function (result) {
+              self.setData({
+                nowInfo: result.data.results[0]
+              })
+            },
+            fail: function ({ errMsg }) {
+              console.log('request fail', errMsg)
+            }
+          })
         }
-      },
-      fail:function(res){
-        me.setData({
-          city:'获取失败'
-        });
-      },
-      complete:function(res){},
-    })
-  },
-  
-      
-  loadCity: function(longitude,latitude){
-    var me = this;
-    wx.request({
-      url: 'https://api.map.baidu.com/geocoder/v2/?ak=1G4Ln9myGQPH95kRxeMi70C4X0zIZ2Z3&location=' + latitude + ',' + longitude + '&output=json',
-      data:{},
-      header:{
-        'Content-Type':'application/json'
-      },
-      success: function(res){
-        if(res && res.data){
-          var city = res.data.result.addressComponent.city;
-          console.log("res..................");
-          console.log(res);
-          me.setData({
-            city:city.indexOf('市') > -1 ? city.substr(0,city.indexOf('市')) :city
-          });
-        }else{
-          me.setData({
-            city:'获取失败'
-          });
-        }
-      }
-    })
-  },
-  
-  
-  onGetUserInfo: function(e) {
-    if (!this.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
-      })
-    }
-  }
-
-  
-})
