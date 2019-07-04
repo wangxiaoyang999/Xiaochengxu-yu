6e208bd3184e6990009315cecf237ffeae43659e
EricXu1 <36099609+EricXu1@users.noreply.github.com>
2019-07-04 10:20:32 +0800

    3.0.1_n
    空气质量阅读

--- miniprogram/pages/aqi/aqi.js
+++ miniprogram/pages/aqi/aqi.js
@@ -1,5 +1,6 @@
 //aqi.js
 //获取应用实例
+var token, IMEI, filePath
 var util_weather = require('../api/util.js')
 var formatLocation = util_weather.formatLocation
 const app = getApp()
@@ -143,4 +144,112 @@ Page({
         }
       }
     })
-  }})
+  },
+    readinfo: function () {
+    var that = this;
+    var grant_type = "client_credentials";
+
+    var appKey = "X230lr6YX2g7hrGgvGgz16kb";
+
+    var appSecret = "UEP0d5CgDrcOPmulfEG6cu44fL7xULzA";
+
+    // var url = "https://openapi.baidu.com/oauth/2.0/token" + "grant_type=" + grant_type + "&client_id=" + appKey + "&client_secret=" + appSecret
+
+    var url = "https://openapi.baidu.com/oauth/2.0/token"
+
+    wx.request({
+
+      url: url,
+
+      data: {
+
+        grant_type: grant_type,
+
+        client_id: appKey,
+
+        client_secret: appSecret
+
+      },
+
+      method: "GET",
+
+      header: {
+
+        'content-type': 'application/json' // 默认值
+
+      },
+
+      success: function (res) {
+
+        console.log(res.data)
+
+        token = res.data.access_token
+
+        var text = that.data.aqi.city.name + ',空气质量指数' + that.data.aqi.aqi.value + ',其中，PM2.5的污染指数为：' + that.data.aqi.aqi.pm25 + '。PM十的污染指数为' + that.data.aqi.aqi.pm10 + '。二氧化氮的污染指数为' + that.data.aqi.aqi.no2;
+
+        var tex = encodeURI(text);//转换编码url_encode UTF8编码
+
+        var tok = token;
+
+        var cuid = IMEI;
+
+        var ctp = 1;
+
+        var lan = "zh";    // zh表示中文
+
+        // 字符编码
+
+        var spd = 5;  // 表示朗读的语速，9代表最快，1是最慢（撩妹请用2，绕口令请用9）
+
+        var url = "https://tsn.baidu.com/text2audio?tex=" + tex + "&lan=" + lan + "&cuid=" + cuid + "&ctp=" + ctp + "&tok=" + tok + "&spd=" + spd
+
+        wx.downloadFile({
+
+          url: url,
+
+          success: function (res) {
+
+            console.log(res)
+
+            filePath = res.tempFilePath;
+
+            // 只要服务器有响应数据，就会把响应内容写入文件并进入 success 回调，业务需要自行判断是否下载到了想要的内容
+
+            if (res.statusCode === 200) {
+
+              wx.playVoice({
+
+                filePath: res.tempFilePath
+
+              })
+
+            }
+            const innerAudioContext = wx.createInnerAudioContext()
+
+            innerAudioContext.autoplay = true
+
+            innerAudioContext.src = encodeURI(filePath)
+
+            innerAudioContext.onPlay(() => {
+
+              console.log('开始播放')
+
+            })
+
+            innerAudioContext.onError((res) => {
+
+              console.log(res.errMsg)
+
+              console.log(res.errCode)
+
+            })
+
+          }
+
+        })
+
+      }
+
+    })
+  }
+  })
